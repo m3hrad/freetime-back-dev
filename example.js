57686d28e109f0f4df11f2efdff6af3ab4df1181
Mahyar <mahyaar.mohammadi@gmail.com>
2017-06-23 17:01:01 +0200

    The first try

---
+++ example.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+http.createServer( (request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello from Lahti\n');
+}).listen(process.env.PORT || 8124);
+console.log('Server running at http://127.0.0.1:8124/');

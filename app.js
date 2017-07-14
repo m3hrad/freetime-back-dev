83a26a0b650d857cb1df9d7e44585d9e175968b8
m3hrad <mehrad.mohammadi@futurice.com>
2017-07-14 20:49:27 +0300

    database added plus the first table

---
+++ app.js
@@ -0,0 +1,22 @@
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/freetime';
+
+const client = new pg.Client(connectionString);
+client.connect();
+const query = client.query(
+  `CREATE TABLE "account" (
+    "id" serial ,
+    "first_name" text,
+    "last_name" text,
+    "email" text,
+    "password_hash" text,
+    "deleted" bool,
+    "phone_number" text,
+    "birthdate" date,
+    "created_on" timestamp with time zone,
+    "photo_url" text,
+    PRIMARY KEY ("id"),
+    UNIQUE ("email"),
+    UNIQUE ("phone_number")
+)`);
+query.on('end', () => { client.end(); });

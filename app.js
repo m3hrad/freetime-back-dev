cd740a4a46e043161c139f2a0768660a4acdb99a
m3hrad <mehrad.mohammadi@futurice.com>
2017-07-15 15:06:04 +0300

    preventing the error if the account table exists

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const pg = require('pg');
+const sys = require('util');
+
 const connectionString = process.env.DATABASE_URL || 'postgres://freetime:freetime@localhost:5432/freetime';
 
 const client = new pg.Client(connectionString);
 client.connect();
+
+
+// const accountTableExistsQuery = client.query(`SELECT * FROM pg_catalog.pg_tables WHERE tablename = 'account'`, function(err, result) {
+//   // done(err);
+//
+//   if(err) {
+//     return console.error('error running query', err);
+//   }
+//   console.log(result.rowCount);
+//
+//   }
+// );
+// console.log('accountTableExistsQuery:');
+// console.log(accountTableExistsQuery);
+
+
+
+
+
 const query = client.query(
-  `CREATE TABLE "account" (
+  `CREATE TABLE IF NOT EXISTS "account" (
     "id" serial ,
     "first_name" text,
     "last_name" text,

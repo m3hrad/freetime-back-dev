9bb8ce1398cc2498451ed2242828ce524fe31447
m3hrad <mehrad.mohammadi@futurice.com>
2017-07-15 16:15:44 +0300

    friends table added

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
 const pg = require('pg');
-const sys = require('util');
 
 const connectionString = process.env.DATABASE_URL || 'postgres://freetime:freetime@localhost:5432/freetime';
 
 const client = new pg.Client(connectionString);
 client.connect();
-
-
-// const accountTableExistsQuery = client.query(`SELECT * FROM pg_catalog.pg_tables WHERE tablename = 'account'`, function(err, result) {
-//   // done(err);
-//
-//   if(err) {
-//     return console.error('error running query', err);
-//   }
-//   console.log(result.rowCount);
-//
-//   }
-// );
-// console.log('accountTableExistsQuery:');
-// console.log(accountTableExistsQuery);
-
-
-
-
 
 const query = client.query(
   `CREATE TABLE IF NOT EXISTS "account" (
@@ -39,5 +20,17 @@ const query = client.query(
     PRIMARY KEY ("id"),
     UNIQUE ("email"),
     UNIQUE ("phone_number")
-)`);
+  );
+  CREATE TABLE IF NOT EXISTS "friends" (
+      "id" integer,
+      "friend_id" integer,
+      PRIMARY KEY ("id"),
+      CONSTRAINT "account_id" FOREIGN KEY ("id") REFERENCES "account"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+      CONSTRAINT "friend_id" FOREIGN KEY ("friend_id") REFERENCES "account"("id") ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+`);
+
+
+
 query.on('end', () => { client.end(); });
